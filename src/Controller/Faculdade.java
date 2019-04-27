fef5a67e0b167abcec0999c9d29e10afba31452c
Flavia <flaviaamin@gmail.com>
2019-04-26 22:52:59 -0300

    Novos metodos

--- src/Controller/Faculdade.java
+++ src/Controller/Faculdade.java
@@ -6,16 +6,46 @@
 package Controller;
 
 import Model.Estudante;
+import java.util.Scanner;
 /**
  *
- * @author LucasCorrea
+ * @author Flavia Amin
  */
 public class Faculdade {
     
     int diaFaculdade = 0;
     
+    Scanner entrada = new Scanner(System.in);
     public void chegouFaculdade(Estudante estudante){
         System.out.println("Você chegou na faculdade!");
+    
+
+        System.out.println("---------Você entra na sala de aula e seu professor saiu, antes de sair------------------------\n"
+                + "---------ele deixou uma equação numérica para que resolvam, seus amigos então fazendo----------\n"
+                + "---------um bolão para quem acertar primeiro a questão, o vencedor ganha 20 reais--------------\n");
+        int resposta;
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("------------------Qual o resultado da equação a seguir: ---------------------");
+        System.out.println("                         Questão 1: 18x = 65 + 43"                                   );
+        resposta = entrada.nextInt();
+
+        if (resposta == 6) {
+            System.out.println("Muito bem, resultado correto, pegue seu dinheiro!");
+            estudante.ganhardinheiro(20);
+            System.out.println("Seu saldo atual é: " + estudante.getDinheiro());
+        } else {
+            System.out.println("Resultado errado, deixou de ganhar 20 reais");
+            
+        }
+
+    }
+
+    public void aula2() {
+
+    }
+
+    public void Ru() {
+
     
     }
     
